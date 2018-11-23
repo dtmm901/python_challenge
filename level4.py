4bc9000b81a8991c52ebe3175750b870e3a75fee
CW <dtmm901@gmail.com>
2018-11-24 00:07:22 +0800

    level 4 complete

--- level4.py
+++ level4.py
@@ -1,3 +1,19 @@
 from urllib.request import urlopen
+import re
 
-url = 'http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=72198'
+
+url = 'http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing={}'
+nothing = 72758
+pattern = re.compile(r'and the next nothing is (\d+)')
+
+while True:
+    html = urlopen(url.format(nothing)).read().decode()
+    print(html)
+    nothing = pattern.search(html)
+    if nothing == None:
+        break
+    nothing = nothing.group(1)
+
+# """
+# 16044, message is "Yes. Divide by two and keep going."
+# """
