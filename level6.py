429d940f6a192f5f695043fcba469eda7ff7fe4f
CW <dtmm901@gmail.com>
2018-11-25 20:45:00 +0800

    level 6 complete

--- level6.py
+++ level6.py
@@ -5,13 +5,18 @@ unzip = zipfile.ZipFile('python challenge/channel.zip')
 # print(unzip.namelist())
 nothing = 90052
 pattern = re.compile(r'Next nothing is (\d+)')
+comments = []
 while True:
     content = unzip.open('{}.txt'.format(nothing)).read().decode()
-    print(content)
+    comments.append(unzip.getinfo('{}.txt'.format(nothing)).comment.decode())
     nothing = pattern.search(content)
     if nothing == None:
         break
     nothing = nothing.group(1)
+print(comments)
+
+print(''.join(comments))
+
 #
 # while True:
 #     file = open('python challenge/channel/{}.txt'.format(nothing)).read()
